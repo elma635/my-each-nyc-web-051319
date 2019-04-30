60707e9f8b96734157b1d410a53d846a80a0262e
Elma Rahman <elmarahman635@gmail.com>
2019-04-30 06:36:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,16 +1,18 @@
 
-def my_each(array)
-i = 0
+# def my_each(array)
+# i = 0
+#
+# while i < array.length
+#
+# yield(array[i])
+#
+# i = i + 1
+#   end
+# end
+#
+# def hello (array)
+#   i = col
 
-while i < array.length
-
-yield(array[i])
-
-i = i + 1
-  end
-end
-
-def hello (array)
 
 
-end
+#end
